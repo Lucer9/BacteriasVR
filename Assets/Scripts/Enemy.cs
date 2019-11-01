2f6eec355cf0db107590fdedb1066e5efcde032b
Carlos A <carlos.aguirreorozco@gmail.com>
2019-11-01 06:03:01 -0600

    Tryhardeo #3

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy: MonoBehaviour {
+
+    GameObject player;
+    NavMeshAgent enemy;
+    Animator anim;
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent <Animator> ();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player == null) {
+            Debug.Log("Did u forgot to tag your player or FPS Controller");
+        }
+
+
+        enemy = GetComponent <NavMeshAgent> ();
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (player != null)
+            enemy.destination = player.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "Player") // this string is your newly created tag
+        {
+            Debug.Log("Did u forgot to tag your player or FPS Controller");
+            anim.SetBool("attack",true);
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Shooting : NetworkBehaviour
+{
+    public int force = 200;
+    public GameObject projectile;
+    public bool cooldown;
+    public float fireRate = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        if(isLocalPlayer){
+//            if ((OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0.0f && !cooldown)||(Input.GetMouseButtonDown(0)) && !cooldown)
+//            {
+//                Fire();
+//                cooldown = true;
+//                Invoke("FireRate", fireRate);
+//            }
+//
+//        }
+    }
+
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+    }
+
+    public void FireRate()
+    {
+        cooldown = false;
+    }
+}
