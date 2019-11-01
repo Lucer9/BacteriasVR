c6a2de9436077ce1104c610efd6daffe87cb35c4
Carlos A <carlos.aguirreorozco@gmail.com>
2019-11-01 06:33:58 -0600

    hace sueño qq

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public  GameObject monster;
+    public float startTime;
+    public float repeatTime;
+    void Start()
+    {
+      InvokeRepeating("Spawn", startTime, repeatTime);
+    }
+
+    void Spawn()
+    {
+        GameObject bullet = Instantiate(monster, transform.position, transform.rotation) as GameObject;
+
+    }
+}
